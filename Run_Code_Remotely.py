3a49aec98941f35ca9145f7c67501d00283b7803
Adam Anouar <43108465+AdamAnouar@users.noreply.github.com>
2023-04-15 01:15:46 +0200

    Update Run_Code_Remotely.py

--- Run_Code_Remotely.py
+++ Run_Code_Remotely.py
@@ -22,6 +22,7 @@ from ghpythonlib.treehelpers import list_to_tree
 #Define a unique id before using sticky
 points_key = 'pts_{}'.format(str(ghenv.Component.InstanceGuid))
 
+#Avoid the component to run into an error
 if points_key not in sticky:
     sticky[points_key] = None
 
@@ -31,7 +32,6 @@ if read:
     folder_path =  os.path.abspath(os.path.dirname(canvas.FilePath))
     with Proxy('contours by color', path = folder_path) as cbc:
         sticky[points_key] = cbc.get_contours(path)
-
 
 #Output for Artist to convert compas geometry into rhino geometry
 pts = sticky[points_key]
